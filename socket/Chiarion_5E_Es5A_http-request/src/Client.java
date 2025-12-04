5f5cbc20ed874fd9473fd697a8f398834a4e254f
danielechiarion <daniele.chiarion@iisviolamarchesini.edu.it>
2025-12-04 13:05:01 +0100

    add client to get a page from a website

---
+++ socket/Chiarion_5E_Es5A_http-request/src/Client.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+public class Client implements Runnable {
+    @Override
+    public void run(){
+        main();
+    }
+
+    public static void main() {
+        final String SERVERIP = "google.it";
+        final int SERVERPORT = 80;
+
+        try{
+            Socket socket = new Socket(SERVERIP, SERVERPORT);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            writer.println("GET / HTTP/1.1");
+            writer.println("Host: google.it");
+            writer.println("Connection: close");
+            writer.println();
+            System.out.println(reader.readLine());
+
+            reader.close();
+            writer.close();
+            socket.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
